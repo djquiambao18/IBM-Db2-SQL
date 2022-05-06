ec802db512f7efdb38ca088f593895fe6ce9f603
Darwish Quiambao <darwishjeric.quiambao@sjsu.edu>
2022-05-05 18:51:27 -0700

    cust login init

--- P2/p2.sql
+++ P2/p2.sql
@@ -28,7 +28,7 @@ LANGUAGE SQL
       SET sql_code = -100;
       SET err_msg = 'Invalid pin';
     ELSE
-      INSERT INTO p2.customer (Name, Gender, Age, Pin) VALUES (p_name, p_gender, p_age, p_pin);
+      INSERT INTO p2.customer (Name, Gender, Age, Pin) VALUES (p_name, p_gender, p_age, p2.encrypt(p_pin));
       SET err_msg = id;
       SET sql_code = 0;
     END IF;
@@ -36,7 +36,21 @@ END@
 --
 
 CREATE PROCEDURE P2.CUST_LOGIN
-(IN p_pin INTEGER, IN p_id INTEGER, OUT )
+(IN p_pin INTEGER, IN p_id INTEGER, OUT valid BINARY, OUT sql_code INTEGER, OUT err_msg CHAR(100))
+LANGUAGE SQL
+  BEGIN
+    IF p_pin < 0 THEN
+      SET sql_code = -100;
+      SET err_msg = 'Invalid pin';
+    ELSEIF p_id < 0 THEN
+      SET sql_code = -100;
+      SET err_msg = 'Invalid id';
+    ELSE
+      SELECT COUNT(*) INTO valid FROM p2.customer WHERE Pin = p2.decrypt(p_pin) AND Id = p_id;
+      SET sql_code = 0;
+      SET err_msg = valid;
+    END IF;
+END@
 TERMINATE@
 --
 --
